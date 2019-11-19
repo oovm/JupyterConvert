698b2a55a21db77d893eab066b5a298f44f796ba
Aster <galaster@foxmail.com>
2019-11-19 22:11:46 +0800

    🌿 Support parse normal cell

--- Packages/Notebook2Jupyter.m
+++ Packages/Notebook2Jupyter.m
@@ -2,6 +2,39 @@
 
 ClearAll["`*"];
 
+(* ::Section:: *)
+(*Normal*)
+
+
+parseCell["Title", data_, co_CellObject] := JupyterMarkdownCell["# " <> parseData@data];
+parseCell["Subtitle", data_, co_CellObject] := JupyterMarkdownCell["## " <> parseData@data];
+parseCell["Chapter", data_, co_CellObject] := JupyterMarkdownCell["### " <> parseData@data];
+parseCell["Section", data_, co_CellObject] := JupyterMarkdownCell["#### " <> parseData@data];
+parseCell["Subsection", data_, co_CellObject] := JupyterMarkdownCell["##### " <> parseData@data];
+parseCell["Subsubsection", data_, co_CellObject] := JupyterMarkdownCell["###### " <> parseData@data];
+
+
+parseCell["Text", data_, co_CellObject] := JupyterMarkdownCell[parseData@data];
+parseCell["WolframAlphaShort", data_String, co_CellObject] := JupyterMarkdownCell[data];
+
+
+(* ::Section:: *)
+(*Code*)
+
+
+parseCell["Input", boxes_, co_CellObject] := Block[
+	{expr = MakeExpression[Cell[boxes], StandardForm], out},
+	out = expr //. {
+		HoldComplete[ExpressionCell[{a___, Null, b___}]] :> StringJoin[ToString[HoldForm@a], ";\n", ToString[HoldForm@b]],
+		HoldComplete[ExpressionCell[a_]] :> ToString[HoldForm@a]
+	};
+	JupyterCodeCell["Input", out]
+];
+parseCell["Output", boxes_, co_CellObject] := Block[
+	{data},
+	data = <|"image/png" -> ExportString[Rasterize@co, {"Base64", "PNG"}]|>;
+	JupyterCodeCell["Output", data]
+];
 
 
 (* ::Chapter:: *)
