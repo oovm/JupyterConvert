18ecf19cdb9cf65e3e28c46b5898f089a403caf9
Aster <galaster@foxmail.com>
2019-12-15 01:11:47 +0800

    🐣 Rewrite paclet extensions

--- Kernel/init.m
+++ Kernel/init.m
@@ -1,4 +1,6 @@
 (* ::Package:: *)
 
+BeginPackage["JupyterConvert`"];
 Get["JupyterConvert`Jupyter2Notebook`"];
 Get["JupyterConvert`Notebook2Jupyter`"];
+EndPackage[]
--- PacletInfo.m
+++ PacletInfo.m
@@ -3,5 +3,18 @@
 Paclet[
 	Name -> "JupyterConvert",
 	Version -> "0.2.0",
-	MathematicaVersion -> "12.0+"
+	MathematicaVersion -> "12.0+",
+	Loading -> Automatic,
+	Extensions -> {
+		{
+			"Kernel",
+			Root -> "Kernel",
+			Context -> {"JupyterConvert`"},
+			Symbols -> {
+				"JupyterConvert`Jupyter2Notebook",
+				"JupyterConvert`Notebook2Jupyter"
+			},
+			HiddenImport -> True
+		}
+	}
 ]
