0e1799b360ffa94eadea6f297cd8c9e33dc396ec
Aster <galaster@foxmail.com>
2019-12-16 19:51:23 +0800

    🌿 Support basic cell maker

--- Kernel/Jupyter2Notebook.m
+++ Kernel/Jupyter2Notebook.m
@@ -4,4 +4,22 @@ Jupyter2Notebook::usage = "";
 
 Begin["`Private`"];
 
+Jupyter2Notebook[in_String, o : OptionsPattern[]] := Block[
+	{out = FileNameJoin[{DirectoryName@in, FileBaseName@in <> ".nb"}]},
+	Jupyter2Notebook[in, out, o]
+];
+
+
+WolframLanguageMake[text_String] := Block[
+	{boxes = BoxData[RowBox[{"Echo", "[", "1", "]"}]]},
+	Cell[boxes, "Input", CellLabel -> "In[ ]:="]
+];
+ExternalLanguageMake[lang_String, text_String] := Cell[
+	text, "ExternalLanguage",
+	CellEvaluationLanguage -> lang,
+	CellLabel -> "In[ ]:="
+];
+EchoMake[text_String] := Cell[BoxData[text], "Echo"]
+OutputMake[] := Cell[BoxData["1"], "Output", CellLabel -> "Out[ ]="];
+
 End[]
