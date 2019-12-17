e437a4c48fe3116e7e64c80087e1b0540cf46893
Aster <galaster@foxmail.com>
2019-12-18 01:36:58 +0800

    🌿 Support jupyter cell parser

--- Kernel/Jupyter2Notebook.m
+++ Kernel/Jupyter2Notebook.m
@@ -4,10 +4,29 @@ Jupyter2Notebook::usage = "";
 
 Begin["`Private`"];
 
+Options[Jupyter2Notebook] = {Debug -> False};
 Jupyter2Notebook[in_String, o : OptionsPattern[]] := Block[
 	{out = FileNameJoin[{DirectoryName@in, FileBaseName@in <> ".nb"}]},
 	Jupyter2Notebook[in, out, o]
 ];
+Jupyter2Notebook[in_String, out_String, o : OptionsPattern[]] := Block[
+	{ipy, lang},
+	ipy = Import[in, "RawJSON"];
+	lang = ipy["metadata", "kernelspec", "name"];
+	lang = Which[
+		StringStartsQ[lang, "python"], "Python",
+		True, "Python"
+	];
+	CellParser[lang] /@ ipy["cells"]
+];
+
+CellParser[lang_String][cell_Association] := Switch[
+	cell["cell_type"],
+	"markdown", MarkdownParser[StringJoin[cell["source"]]],
+	"code", CodeParser[lang, StringJoin[cell["source"]], cell["outputs"]]
+];
+
+
 
 
 WolframLanguageMake[text_String] := Block[
@@ -19,7 +38,7 @@ ExternalLanguageMake[lang_String, text_String] := Cell[
 	CellEvaluationLanguage -> lang,
 	CellLabel -> "In[ ]:="
 ];
-EchoMake[text_String] := Cell[BoxData[text], "Echo"]
+EchoMake[text_String] := Cell[BoxData[text], "Echo"];
 OutputMake[] := Cell[BoxData["1"], "Output", CellLabel -> "Out[ ]="];
 
 End[]
